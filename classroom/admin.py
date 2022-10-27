7163cc08f6d7d5f272ef12a55190637f6b1f7f66
sarbazzz <116829120+sarbazzz@users.noreply.github.com>
2022-10-27 22:26:02 +0600

    Add files via upload

---
+++ classroom/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+admin.site.register(User)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Student)
+admin.site.register(TakenQuiz)
+admin.site.register(StudentAnswer)
